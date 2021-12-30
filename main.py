08a9074e9312554f2533c4a082b65743dafdff7e
itosun <ismailtosuntnyl@gmail.com>
2021-12-31 00:01:22 +0300

    final

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+import time
 from dotenv import dotenv_values
 from database import Database
 import os
 
+intro ="""
 
+______      _       ______                   ___              
+|  _  \    | |      | ___ \                 / _ \             
+| | | |__ _| |_ __ _| |_/ / __ _ ___  ___  / /_\ \_ __  _ __  
+| | | / _` | __/ _` | ___ \/ _` / __|/ _ \ |  _  | '_ \| '_ \ 
+| |/ / (_| | || (_| | |_/ / (_| \__ \  __/ | | | | |_) | |_) |
+|___/ \__,_|\__\__,_\____/ \__,_|___/\___| \_| |_/ .__/| .__/ 
+                                                 | |   | |    
+                                                 |_|   |_|    
+                                                                                  
+"""
+outro = """ 
+ _____                               
+/  ___|                              
+\ `--.  ___  ___   _   _  ___  _   _ 
+ `--. \/ _ \/ _ \ | | | |/ _ \| | | |
+/\__/ /  __/  __/ | |_| | (_) | |_| |
+\____/ \___|\___|  \__, |\___/ \__,_|
+                    __/ |            
+                   |___/             
+                                     
+                       
+"""
 def print_database(object):
     os.system('cls')
-    print(f"Executed query is '{object[2]}'")
+    print(f"Executed query is '{object[2]}'\n")
     header = ""
     for title in object[1]:
         header += "|     " + title + "    |"
-    print(header)
+    print(header+"\n")
     for row in object[0]:
         s = ""
         for column in row:
@@ -23,10 +47,10 @@ def menu(db):
                 "2.Özel Sorgu\n" \
                 "0.Çıkış"
 
-
     while True:
 
         try:
+            os.system('cls')
             print(main_menu)
             input_variable = int(input("Yapmak istediğiniz işlemin numarasını giriniz: "))
             os.system('cls')
@@ -49,7 +73,7 @@ def menu(db):
                     os.system('cls')
                     print("Hatalı Tuşlama Yaptınız")
                     input("\n Devam Etmek için 'ENTER' basın")
-        except :
+        except:
             os.system('cls')
             print("Hatalı Tuşlama Yaptınız")
             db = Database(dotenv_values()["url"])
@@ -57,5 +81,10 @@ def menu(db):
 
 
 if __name__ == '__main__':
+    print(intro)
+    time.sleep(3)
+    # pass  your database url here
     db = Database(dotenv_values()["url"])
     menu(db)
+    print(outro)
+    time.sleep(3)
